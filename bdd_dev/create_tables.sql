71d4a028c3dc8e0eb863907e051bcf62ecd86467
Alix Danvy <alix.danvy@ens-rennes.fr>
2024-02-02 19:43:49 +0100

    bon avancement sur la création des tables

--- bdd_dev/create_tables.sql
+++ bdd_dev/create_tables.sql
@@ -1,47 +1,33 @@
-CREATE TABLE IF NOT EXISTS acheteur (
-    id_acheteur INT PRIMARY KEY,
-    nom VARCHAR(50)
+CREATE TABLE IF NOT EXISTS titulaire (
+    id_titulaire INT PRIMARY KEY,
+    denomination_sociale VARCHAR(128),
+    type_identifiant VARCHAR(128)
 );
 
-CREATE TABLE IF NOT EXISTS nature (
+CREATE TABLE IF NOT EXISTS procedure (
+    procedure_id INT PRIMARY KEY,
+    procedure_name VARCHAR(70)
+);
+
+CREATE TABLE IF NOT EXISTS nature {
     id_nature INT PRIMARY KEY,
     nature VARCHAR(30)
-);
+};
 
 CREATE TABLE IF NOT EXISTS forme_Prix (
     id_forme INT PRIMARY KEY,
     forme VARCHAR(30)
 );
 
-CREATE TABLE IF NOT EXISTS procedure (
-    procedure_id INT PRIMARY KEY,
-    procedure_name VARCHAR(40)
+CREATE TABLE IF NOT EXISTS acheteur (
+    id_acheteur INT PRIMARY KEY,
+    nom VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS transactions (
-    id INT PRIMARY KEY,
-    objet TEXT,
-    id_modif INT,
-    lieu_exec INT,
-    source VARCHAR(30),
-    nature INT,
-    forme_prix INT,
-    date_notif DATE,
-    date_public_donnee DATE,
-    duree_mois INT,
-    type INT,
-    procedure_id INT,
-    montant INT,
-    FOREIGN KEY (nature) REFERENCES Nature(id_nature),
-    FOREIGN KEY (forme_prix) REFERENCES Forme_Prix(id_forme),
-    FOREIGN KEY (type) REFERENCES Type_transaction(id) -- is there for later
-);
-
-CREATE TABLE IF NOT EXISTS titulaire (
-    id_titulaire INT PRIMARY KEY,
-    denomination_sociale VARCHAR(128),
-    type_denomination VARCHAR(128)
-);
+CREATE TABLE IF NOT EXISTS source {
+    id_source INT PRIMARY KEY,
+    source VARCHAR(30)
+};
 
 CREATE TABLE IF NOT EXISTS lieu (
     type_code INT,
@@ -52,6 +38,47 @@ CREATE TABLE IF NOT EXISTS lieu (
     code_dpt INT,
     code_region INT
 );
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT PRIMARY KEY,
+    -- not null:
+    FOREIGN KEY (id_titulaire_1) REFERENCES titulaire(id_titulaire),
+    FOREIGN KEY (id_titulaire_2) REFERENCES titulaire(id_titulaire),
+    FOREIGN KEY (id_titulaire_3) REFERENCES titulaire(id_titulaire),
+    FOREIGN KEY (procedure_id) REFERENCES procedure(procedure_id),
+    FOREIGN KEY (nature) REFERENCES nature(id_nature),
+    codeCPV VARCHAR(20), -- 14 is the max length in the dataset but it's variable...
+    duree_mois INT,
+    montant FLOAT,
+    FOREIGN KEY (forme_prix) REFERENCES Forme_Prix(id_forme),
+    date_notif DATE,
+    objet TEXT,
+    FOREIGN KEY (id_acheteur) REFERENCES acheteur(id_acheteur),
+    FOREIGN KEY (source) REFERENCES source(id_source),
+
+    -- HERE
+
+
+    id_modif INT,
+    lieu_exec INT,
+    source VARCHAR(30),
+    forme_prix INT,
+    date_public_donnee DATE,
+    type INT,
+    montant INT,
+    FOREIGN KEY (type) REFERENCES Type_transaction(id) -- is there for later
+);
+
+CREATE TABLE IF NOT EXISTS nature (
+    id_nature INT PRIMARY KEY,
+    nature VARCHAR(30)
+);
+
+
+
+
+
+
 
 -- This table will be extended when we will have the llm preprocessing about object, which is in natural language
 CREATE TABLE IF NOT EXISTS type_transaction (
