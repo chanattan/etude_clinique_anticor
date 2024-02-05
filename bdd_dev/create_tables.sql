afdeef216044e7ede8691352f46c94c35306afd8
Alix Danvy <alix.danvy@ens-rennes.fr>
2024-02-05 11:27:00 +0100

    tables almost defined, remains the json cases to work

--- bdd_dev/create_tables.sql
+++ bdd_dev/create_tables.sql
@@ -6,38 +6,56 @@ CREATE TABLE IF NOT EXISTS titulaire (
 
 CREATE TABLE IF NOT EXISTS procedure (
     procedure_id INT PRIMARY KEY,
-    procedure_name VARCHAR(70)
+    procedure_name VARCHAR(128)
 );
 
 CREATE TABLE IF NOT EXISTS nature {
     id_nature INT PRIMARY KEY,
-    nature VARCHAR(30)
+    nature VARCHAR(32)
 };
 
 CREATE TABLE IF NOT EXISTS forme_Prix (
     id_forme INT PRIMARY KEY,
-    forme VARCHAR(30)
+    forme VARCHAR(32)
 );
 
 CREATE TABLE IF NOT EXISTS acheteur (
     id_acheteur INT PRIMARY KEY,
-    nom VARCHAR(255)
+    nom VARCHAR(256)
 );
 
 CREATE TABLE IF NOT EXISTS source {
     id_source INT PRIMARY KEY,
-    source VARCHAR(30)
+    source VARCHAR(32)
 };
 
 CREATE TABLE IF NOT EXISTS lieu (
-    type_code INT,
+    id_lieu INT PRIMARY KEY,
+    code VARCHAR(128), -- unfortunately, it happens that the code is a string and not well defined. This will be hopefully taken care of in the pre/post processing of the data with llm
     nom VARCHAR(128),
-    code_postal INT,
-    code_commune INT,
-    code_pays INT,
-    code_dpt INT,
-    code_region INT
+    typeCode VARCHAR(64) -- could be a tuple, but we might have surprises later....
+                         -- also, we might want to have a table for the typeCode, but it begins to be a bit too much
 );
+
+CREATE TABLE IF NOT EXISTS modalite_execution (
+    id_modalite INT PRIMARY KEY,
+    modalite VARCHAR(32) -- could be a tuple, but we might have surprises later....
+);
+
+CREATE TABLE IF NOT EXISTS type_prix (
+    id_type_prix INT PRIMARY KEY,
+    type VARCHAR(32) -- could be a tuple, but it seems that we don't have all the possible descriptions possible in the law from the tables 
+);
+
+CREATE TABLE IF NOT EXISTS considerations_sociales (
+    id_considerations_sociales INT PRIMARY KEY,
+    considerations_sociales VARCHAR(32) -- could be a tuple, but it seems that we don't have all the possible descriptions possible in the law from the tables
+)
+
+CREATE TABLE IF NOT EXISTS considerations_environnementales (
+    id_considerations_env INT PRIMARY KEY,
+    considerations_env VARCHAR(32) -- could be a tuple, but it seems that we don't have all the possible descriptions possible in the law from the tables
+)
 
 CREATE TABLE IF NOT EXISTS transactions (
     id INT PRIMARY KEY,
@@ -53,9 +71,34 @@ CREATE TABLE IF NOT EXISTS transactions (
     FOREIGN KEY (forme_prix) REFERENCES Forme_Prix(id_forme),
     date_notif DATE,
     objet TEXT,
-    FOREIGN KEY (id_acheteur) REFERENCES acheteur(id_acheteur),
+    FOREIGN KEY (acheteur) REFERENCES acheteur(id_acheteur),
     FOREIGN KEY (source) REFERENCES source(id_source),
+    FOREIGN KEY (lieu_exec) REFERENCES lieu(id_lieu),
+    date_publication_donnees DATE,
+    date_creation DATE,
+    FOREIGN KEY (id_modalite) REFERENCES modalite_execution(id_modalite),
+    marche_inovant BOOLEAN,
+    -- cahiers des clauses administratives générales
+    ccag VARCHAR(128),
+    offres_recues INT,
+    attribution_avance BOOLEAN,
+    type_groupement_operateurs ENUM('Conjoint','Solidaire'),
+    FOREIGN KEY (type_prix) REFERENCES type_transaction(id_type_prix),
+    FOREIGN KEY (considerations_sociales) REFERENCES considerations_sociales(id_considerations),
+    FOREIGN KEY (considerations_env) REFERENCES considerations_environnementales(id_considerations_env),
+    taux_avance FLOAT,
+    origine_ue FLOAT, -- don't know what it is
+    origine_france FLOAT, -- still
+    update_at DATE, -- don't know what it is
+    FOREIGN KEY (id_modif) REFERENCES modification(id_modif),
+    technique BOOLEAN, -- Accord cadre or not. Peraps should rename
+    id_accord_cadre VARCHAR(64), -- supposed to be an id but it's a string and sometimes with a sentence in it....
+    
+    
 
+
+
+    sous_traitance_declaree BOOLEAN,
     -- HERE
 
 
@@ -63,21 +106,22 @@ CREATE TABLE IF NOT EXISTS transactions (
     lieu_exec INT,
     source VARCHAR(30),
     forme_prix INT,
-    date_public_donnee DATE,
     type INT,
     montant INT,
     FOREIGN KEY (type) REFERENCES Type_transaction(id) -- is there for later
 );
 
-CREATE TABLE IF NOT EXISTS nature (
-    id_nature INT PRIMARY KEY,
-    nature VARCHAR(30)
+CREATE TABLE IF NOT EXISTS modification (
+    id_modif INT PRIMARY KEY,
+    id_modification BOOLEAN, -- for some reason, it's a boolean in the dataset but not clear what it is and rarely used
+    objet_modification TEXT,
+    duree_mois INT,
+    montant INT,
+    date_modification DATE,
+    date_notif DATE,
+
+    -- problem with titulaire_modification, it's a json of titulaire objects but of variable length
 );
-
-
-
-
-
 
 
 -- This table will be extended when we will have the llm preprocessing about object, which is in natural language
@@ -86,15 +130,3 @@ CREATE TABLE IF NOT EXISTS type_transaction (
     -- Define your enum values here, e.g., ENUM('value1', 'value2', 'value3')
 );
 
-CREATE TABLE IF NOT EXISTS modification (
-    id_modification INT PRIMARY KEY,
-    montant INT,
-    date DATE,
-    objet VARCHAR(256),
-    type INT,
-    duree_mois INT,
-    titulaire INT,
-    date_modification DATE,
-    FOREIGN KEY (type) REFERENCES Type_transaction(id),
-    FOREIGN KEY (titulaire) REFERENCES Titulaire(id_titulaire)
-);
